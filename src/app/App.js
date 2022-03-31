f418f3c9c5fff3a7b1480c84c1be577e1dd8e97f
Simona Pizio <simona.pizio@gmail.com>
2022-03-31 16:08:18 +0200

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import {Inventory} from '../features/inventory/Inventory';
+import { Cart } from '../features/cart/Cart';
+import { SearchTerm } from '../features/searchTerm/SearchTerm';
+import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter';
+
+export default function App(props) {
+  const { state, dispatch } = props;
+
+  function getFilteredItems(items, searchTerm) {
+    return items.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+
+  const visibleInventory = getFilteredItems(state.inventory, state.searchTerm);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchTerm 
+        searchTerm={state.searchTerm}
+        dispatch={dispatch}
+      />
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Inventory
+        inventory={visibleInventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+      <Cart 
+        cart={state.cart}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
     </div>
   );
-}
-
-export default App;
+};
