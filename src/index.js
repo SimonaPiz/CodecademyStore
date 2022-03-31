2cac5383c3c9f0ed1e2400f56d5810ae13a22751
Simona Pizio <simona.pizio@gmail.com>
2022-03-31 15:26:41 +0200

    Moved App in new directory and Add store for redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
+import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+      state={store.getState()}
+      dispatch={store.dispatch}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
