502ca0037df35564dfa6d86068c2c66182019ed5
Simona Pizio <simona.pizio@gmail.com>
2022-03-31 16:11:44 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 export const inventoryData = [
   { name: 'Hello World Hat', price: 23.99, img: 'img/hello-world-hat.jpg' },
-  { name: 'Learn From Home Joggers', price: 45.99, img: 'img/sweatpants.jpg' },
-  { name: 'Java Tee', price: 17.99, img: 'img/java-tee.jpg' },
-  { name: 'Python Tee', price: 17.99, img: 'img/python-tee.jpg' },
-  { name: 'SQL Tee', price: 17.99, img: 'img/sql-tee.jpg' },
-  { name: 'Hello World Hoodie', price: 49.99, img: 'img/hoodie.jpg' },
+  { name: 'Learn From Home Joggers', price: 45.99, img: 'img/sweatpants.webp' },
+  { name: 'Java Tee', price: 17.99, img: 'img/java-tee.webp' },
+  { name: 'Python Tee', price: 17.99, img: 'img/python-tee.webp' },
+  { name: 'SQL Tee', price: 17.99, img: 'img/sql-tee.webp' },
+  { name: 'Hello World Hoodie', price: 49.99, img: 'img/hoodie.webp' },
 ];
 
 export const currenciesData = ['USD', 'EUR', 'CAD'];
